5e4ac80d68cb87d272becf9e99d962e6a64d4751
Tim Holmes <timothy.holmes@homes.com>
2017-02-20 16:33:28 -0500

    Converted rulebooks to SQL

--- util/sqlSetup.sql
+++ util/sqlSetup.sql
@@ -6,17 +6,17 @@ CREATE DATABASE olympia_dev;
 /* Create tables */
 CREATE TABLE olympia_dev.olympians (id SERIAL PRIMARY KEY, name STRING, description STRING);
 CREATE TABLE olympia_dev.coliseums (id SERIAL PRIMARY KEY, name STRING, description STRING);
-CREATE TABLE olympia_dev.gametypes (id SERIAL PRIMARY KEY, name STRING, point_types STRING);
+CREATE TABLE olympia_dev.rulebooks (id SERIAL PRIMARY KEY, name STRING, point_types STRING);
 CREATE TABLE olympia_dev.olympian_stats (
     id SERIAL PRIMARY KEY,
     fk_olympian_id INT NOT NULL REFERENCES olympia_dev.olympians (id),
     fk_coliseum_id INT NOT NULL REFERENCES olympia_dev.coliseums (id),
-    fk_gametype_id INT NOT NULL REFERENCES olympia_dev.gametypes (id),
+    fk_gametype_id INT NOT NULL REFERENCES olympia_dev.rulebooks (id),
     stat_object STRING
 );
 CREATE TABLE olympia_dev.coliseum_stats (
     id SERIAL PRIMARY KEY,
     fk_coliseum_id INT NOT NULL REFERENCES olympia_dev.coliseums (id),
-    fk_gametype_id INT NOT NULL REFERENCES olympia_dev.gametypes (id),
+    fk_gametype_id INT NOT NULL REFERENCES olympia_dev.rulebooks (id),
     stat_object STRING
 );
