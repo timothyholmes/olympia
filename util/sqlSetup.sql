1099cf56c60b26b00f4b118b508990001c8035f9
Tim Holmes <timothy.holmes@homes.com>
2017-02-21 13:22:56 -0500

    Did a lot of work for the match endpoint. Abstracted some of the queries and moved them out of the handlers so they could be reused in other parts of the API.

--- util/sqlSetup.sql
+++ util/sqlSetup.sql
@@ -1,9 +1,5 @@
 
-/* Create databases */
-CREATE DATABASE olympia;
 CREATE DATABASE olympia_dev;
-
-/* Create tables */
 CREATE TABLE olympia_dev.olympians (id SERIAL PRIMARY KEY, name STRING, description STRING);
 CREATE TABLE olympia_dev.coliseums (id SERIAL PRIMARY KEY, name STRING, description STRING);
 CREATE TABLE olympia_dev.rulebooks (id SERIAL PRIMARY KEY, name STRING, point_types STRING);
@@ -13,6 +9,12 @@ CREATE TABLE olympia_dev.olympian_stats (
     fk_coliseum_id INT NOT NULL REFERENCES olympia_dev.coliseums (id),
     fk_gametype_id INT NOT NULL REFERENCES olympia_dev.rulebooks (id),
     stat_object STRING
+);
+CREATE TABLE olympia_dev.matches (
+    id SERIAL PRIMARY KEY,
+    fk_coliseum_id INT NOT NULL REFERENCES olympia_dev.coliseums (id),
+    fk_gametype_id INT NOT NULL REFERENCES olympia_dev.rulebooks (id),
+    match_object STRING
 );
 CREATE TABLE olympia_dev.coliseum_stats (
     id SERIAL PRIMARY KEY,
